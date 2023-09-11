45184fd84bd4e9812c60b997c9383758720cf2b7
Ashley Harrop <144391801+ashleyharrop@users.noreply.github.com>
2023-09-11 11:36:16 -0400

    Initial

--- sketch.js
+++ sketch.js
@@ -1,7 +1,15 @@
 function setup() {
   createCanvas(400, 400);
+  rectMode(CENTER);
+  fill("red")
 }
 
 function draw() {
-  background(125);
+  background(255,125,0);
+
+  fill("red");
+  rect(200, 200, 100, 100);
+
+  fill("green");
+  ellipse(30, 30, 30);
 }
