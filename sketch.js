51a0976418e3636faa60d88875b54fd1199227a4
Ashley Harrop <144391801+ashleyharrop@users.noreply.github.com>
2023-09-12 22:19:04 -0400

    4th commit

--- sketch.js
+++ sketch.js
@@ -1,15 +1,36 @@
 function setup() {
   createCanvas(400, 400);
   rectMode(CENTER);
-  fill("red")
+  background(128,0,128);
+  
+
 }
 
 function draw() {
-  background(200,200,0);
+  fill(230,0,230);
+  rect(90,300,150,150);
 
-  fill("red");
-  rect(200, 200, 100, 100);
+  fill(255,102,255);
+  rect(90,300,100,100);
 
-  rect(50, 50);
+  fill(255,230,255);
+  rect(90,300,50,50);
+
+  fill(230,0,230);
+  rect(300,90,150,150);
+
+  fill(255,102,255);
+  rect(300,90,100,100);
+
+  fill(255,230,255);
+  rect(300,90,50,50);
+
+  fill(255,255,102);
+  rect(200,200,5,400);
+
+  fill(255,255,102);
+  rect(200,200,400,5);
 
 }
+ 
+
